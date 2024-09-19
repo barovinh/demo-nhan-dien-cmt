56d83028385d30748c84289601372a741c6f825c
deethesaint <vtduong0912@gmail.com>
2024-09-19 14:35:00 +0700

    edit

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
+    $("#input-file").change(function(e) {
+        for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+            var file = e.originalEvent.srcElement.files[i];
+    
+            var img = document.createElement("img");
+            var reader = new FileReader();
+            reader.onloadend = function() {
+                 img.src = reader.result;
+                 img.width = 200;
+                 img.height = 200;
+            }
+            reader.readAsDataURL(file);
+            $("#input-file").after(img);
+            var label = document.createElement("p");
+            label.innerHTML = "Ảnh đã tải lên: "
+            img.before(label)
+        }
+    });
+
     $('#submit').on("click", function(){
         var formData = new FormData();
-        var imageFile = $('#input-file')[0].files[0]; // Assuming you have an input field for image
+        var imageFile = $('#input-file')[0].files[0];
+        if (!imageFile) {
+            alert('Vui lòng chọn ảnh!')
+            return;
+        }
         formData.append("image", imageFile);
-
+        formData.append("face", 1);
+        $('#loading').removeClass('d-none')
         $.ajax({
             url: "https://api.fpt.ai/vision/idr/vnm",
             type: "POST",
@@ -14,10 +38,24 @@ $(document).ready(function(){
                 'api_key': "SYe4cuLbzfvZW4iEBSlZWg51HFTwRrH2"
             },
             success: function(data, status){
-                alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+                // alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+                console.log(data)
+                var rd = data.data[0]
+                $('#info').empty()
+                $('#info').append('<p>MÃ CCCD: ' + '<span class="fw-bold">' + rd.id + '</span>' + '</p>')
+                $('#info').append('<p>HỌ VÀ TÊN: ' + '<span class="fw-bold">' + rd.name + '</span>' + '</p>')
+                $('#info').append('<p>NGÀY THÁNG NĂM SINH: ' + '<span class="fw-bold">' + rd.dob + '</br>' + '</p>')
+                $('#info').append('<p>GIỚI TÍNH: ' + '<span class="fw-bold">' + rd.sex + '</span>' + '</p>')
+                $('#info').append('<p>QUỐC TỊCH: ' + '<span class="fw-bold">' + rd.nationality + '</br>' + '</p>')
+                $('#info').append('<p>QUÊ QUÁN: ' + '<span class="fw-bold">' + rd.home + '</span>' + '</p>')
+                $('#info').append('<p>Đ/C THƯỜNG TRÚ: ' + '<span class="fw-bold">' + rd.address + '</span>' + '</p>')
+                $('#info').append('<p>NGÀY HẾT HẠN: ' + '<span class="fw-bold">' + rd.doe + '</span>' + '</p>')
+                $('#info').append('<p>HÌNH ẢNH KHUÔN MẶT: </p>')
+                $('#info').append('<img width="155" height="187" alt="" src="data:image/png;base64,' + rd.face_base64 + '">')
+                $('#loading').addClass('d-none')
             },
             error: function(xhr, status, error){
-                alert("Error: " + error + "\nStatus: " + status);
+                alert('Ảnh không đủ tiêu chuẩn nhận diện (Mờ, thiếu góc, thiếu thông tin, không phải CCCD Việt Nam,...)')
             }
         });
     });
